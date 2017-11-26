6fa302ae73b8ce483928d4744b931671bab53f03
amit chanchal <amit.chanchal@hotmail.com>
2017-11-26 14:13:09 +0530

    lesson1

---
+++ lesson1.js
@@ -0,0 +1,16 @@
+/* 
+    This program is just a demonstration for running a js file using node. this is part of enable courses
+    1. http://enable.sustainurban.org/courses/computation-and-design/introduction-to-programming/
+    2. http://enable.sustainurban.org/courses/architecture-and-design/automation-and-built-environment/
+
+    Author: Amit Chanchal
+    file: lesson1.js (hello world)
+
+*/
+// Replace enable with your name
+my_name = 'enable';
+console.log("Hello everyone, I am "+ my_name);
+
+// save the file in your system and navigate to that directory 
+// use command node filename.js <enter>
+
